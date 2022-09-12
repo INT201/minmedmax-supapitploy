779c5729f0792a860f438852ea32290fe451b20c
supapitploy <88095690+supapitploy@users.noreply.github.com>
2022-09-12 17:29:56 +0700

    Update minMedMax.js

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,13 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+  let min = Math.min(n1, n2, n3);
+    let max = Math.max(n1, n2, n3);
+    let mid = (n1 + n2 + n3) - (min + max);
+    return {
+        min: min,
+        mid: mid,
+        max: max
+  }
 }
 module.exports = minMedMax
